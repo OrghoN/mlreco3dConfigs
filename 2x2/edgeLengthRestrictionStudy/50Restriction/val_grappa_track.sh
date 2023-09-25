bdc4033749964a35a48d3cf8dbceb06b364fa435
Orgho Neogi <anoronyo@sdfiana007.sdf.slac.stanford.edu>
2023-09-25 15:24:15 -0700

    slearn files for running train and val

---
+++ 2x2/edgeLengthRestrictionStudy/50Restriction/val_grappa_track.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#SBATCH --account=neutrino
+#SBATCH --partition=ampere
+
+#SBATCH --job-name=val_grappa_track
+#SBATCH --output=batch_outputs/output-val_grappa_track.txt 
+#SBATCH --error=batch_outputs/output-val_grappa_track.txt
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=4g
+#SBATCH --time=36:00:00 
+#SBATCH --gpus a100:1
+
+singularity exec --bind $APP_DIR,/sdf/data/neutrino/kterao/dunend_train_prod/ --nv /sdf/group/neutrino/images/develop.sif bash -c "python3 $MLRECO3D_PATH/bin/run.py grappa_track_val.cfg"
