2ef213f29f5bd5ba500a2a720d7c9ed7f02b220a
Orgho Neogi <anoronyo@sdfiana007.sdf.slac.stanford.edu>
2023-09-25 15:53:29 -0700

    slearn config for no edge length restriction

---
+++ 2x2/edgeLengthRestrictionStudy/noRestriction/train_grappa_shower.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#SBATCH --account=neutrino
+#SBATCH --partition=ampere
+
+#SBATCH --job-name=train_grappa_shower
+#SBATCH --output=batch_outputs/output-train_grappa_shower.txt 
+#SBATCH --error=batch_outputs/output-train_grappa_shower.txt
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=4g
+#SBATCH --time=3-00:00:00 
+#SBATCH --gpus a100:1
+
+singularity exec --bind $APP_DIR,/sdf/data/neutrino/kterao/dunend_train_prod/ --nv /sdf/group/neutrino/images/develop.sif bash -c "python3 $MLRECO3D_PATH/bin/run.py grappa_shower.cfg"
