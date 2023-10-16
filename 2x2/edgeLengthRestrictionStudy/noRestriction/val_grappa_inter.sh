2253bc8560989c2b7692c1128e92cafc8f51081c
Orgho Neogi <anoronyo@sdfiana008.sdf.slac.stanford.edu>
2023-10-16 10:48:00 -0700

    Extended time for track and inter

--- 2x2/edgeLengthRestrictionStudy/noRestriction/val_grappa_inter.sh
+++ 2x2/edgeLengthRestrictionStudy/noRestriction/val_grappa_inter.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=4g
-#SBATCH --time=36:00:00 
+#SBATCH --time=5-00:00:00 
 #SBATCH --gpus a100:1
 
 singularity exec --bind $APP_DIR,/sdf/data/neutrino/kterao/dunend_train_prod/ --nv /sdf/group/neutrino/images/develop.sif bash -c "python3 $MLRECO3D_PATH/bin/run.py grappa_inter_val.cfg"
